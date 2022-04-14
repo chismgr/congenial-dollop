e15f7e6087afa22f9909d3f0054f20d3a75880c3
Garrett Chism <chismgr@umich.edu>
2022-04-14 12:21:53 -0400

    Updates from posneg and mixdup

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class TestSelectionSort {
 	private SelectionSort temp;
-	
+
 	public void test() {
 		testPositive();
 		testNegative();
@@ -24,20 +24,20 @@ public class TestSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         temp = new SelectionSort();
-        
+
         arr=temp.basicSelectionSort(arr);
-        
+
         System.out.println("Positive: " + Arrays.toString(arr));
-        
+
         assertArrayEquals(Sortedarr, arr);
     }
     @Test
@@ -45,50 +45,50 @@ public class TestSelectionSort {
     	int[] arr = new int[5];
         arr[0] = -12;
         arr[1] = -6;
-        arr[2] = -12324;
+        arr[2] = -1324;
         arr[3] = -1;
         arr[4] = -145;
-        
+
         int[] Sortedarr = new int[5];
-        Sortedarr[0] = -12324;
+        Sortedarr[0] = -1324;
         Sortedarr[1] = -145;
         Sortedarr[2] = -12;
         Sortedarr[3] = -6;
         Sortedarr[4] = -1;
-        
+
         temp = new SelectionSort();
-        
+
         arr=temp.basicSelectionSort(arr);
-        
+
         System.out.println();
         System.out.println("Negative: " + Arrays.toString(arr));
-        
+
         assertArrayEquals(Sortedarr, arr);
     }
     @Test
     public void testMixed(){
     	int[] arr = new int[5];
-        arr[0] = -888;
+        arr[0] = -88;
         arr[1] = 49;
-        arr[2] = 9847;
+        arr[2] = 984;
         arr[3] = -1;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
-        Sortedarr[0] = -888;
+        Sortedarr[0] = -88;
         Sortedarr[1] = -1;
         Sortedarr[2] = 2;
         Sortedarr[3] = 49;
-        Sortedarr[4] = 9847;
-        
+        Sortedarr[4] = 984;
+
         temp = new SelectionSort();
-        
+
         arr=temp.basicSelectionSort(arr);
-        
+
         System.out.println("Mixed: " + Arrays.toString(arr));
         System.out.println();
-        
-        assertArrayEquals(Sortedarr, arr);  
+
+        assertArrayEquals(Sortedarr, arr);
     }
     @Test
     public void testDuplicates(){
@@ -98,21 +98,21 @@ public class TestSelectionSort {
         arr[2] = 89;
         arr[3] = -27;
         arr[4] = 0;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -27;
         Sortedarr[1] = -27;
         Sortedarr[2] = 0;
         Sortedarr[3] = 89;
         Sortedarr[4] = 89;
-        
+
         temp = new SelectionSort();
-        
+
         arr=temp.basicSelectionSort(arr);
-        
+
         System.out.println("Dublicates: " + Arrays.toString(arr));
         System.out.println();
-        
+
         assertArrayEquals(Sortedarr, arr);
     }
 }
